51a7b705a4605ed4df2afeddefea1388f8ba4c47
IoannisMorakis <yannimorakis@hotmail.com>
2024-01-29 21:27:24 -0400

    commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-    projectId: 'gestion-de-muestras-de-c-fabe0',
-    appId: '1:881625861704:web:1da279954953bf7e3ad27d',
-    databaseURL:'https://gestion-de-muestras-de-c-fabe0-default-rtdb.firebaseio.com',
-    storageBucket: 'gestion-de-muestras-de-c-fabe0.appspot.com',
-    apiKey: 'AIzaSyD0Py3MA2BD1yEsMg4wM9RDhbNgmEAxZPk',
-    authDomain: 'gestion-de-muestras-de-c-fabe0.firebaseapp.com',
-    messagingSenderId: '881625861704',
-    measurementId: 'G-PTHV8B640J',
+    projectId: 'gestion-de-muestras-de-campo',
+    appId: '1:956362829056:web:235ab7e9c2cbd05ffe97e5',
+    databaseURL: 'https://gestion-de-muestras-de-campo-default-rtdb.firebaseio.com',
+    storageBucket: 'gestion-de-muestras-de-campo.appspot.com',
+    apiKey: 'AIzaSyDeImqmNn3CZpIAB1A9_q78bnqPVNM1ji8',
+    authDomain: 'gestion-de-muestras-de-campo.firebaseapp.com',
+    messagingSenderId: '956362829056',
+    measurementId: 'G-T5SRLVHLR3',
 
   },
   production: false
