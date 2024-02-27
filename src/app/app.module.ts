e04b6dbf7eb2bf924c6814dfff41380185ee86bb
IoannisMorakis <yannimorakis@hotmail.com>
2024-02-27 00:44:20 -0400

    PWA-test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,73 +1,16 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { AuthModule } from './auth/auth.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirebaseApp, initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore, enableIndexedDbPersistence, connectFirestoreEmulator, persistentLocalCache, persistentSingleTabManager} from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { UserModule } from './user/user.module';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-import { AdminModule } from './admin/admin.module';
-import { PrintComponent } from './print/print.component';
-
-
-
-import { NgxScannerQrcodeModule, LOAD_WASM } from 'ngx-scanner-qrcode';
-import { SafePipe } from './safe.pipe';
-import { RouterModule } from '@angular/router';
-import { ClientModule } from './client/client.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-
-LOAD_WASM().subscribe((res: any) => console.log('LOAD_WASM', res))
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PrintComponent,
-    SafePipe
+    AppComponent
   ],
   imports: [
-    RouterModule,
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    AuthModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => //getFirestore()),
-      {
-        const firestore = getFirestore();
-        //connectFirestoreEmulator(firestore, 'localhost', 8080);
-        enableIndexedDbPersistence(firestore);
-        //persistentLocalCache({ tabManager: persistentSingleTabManager({}) });
-        return firestore;
-      }),
-    provideFunctions(() => getFunctions()),
-    UserModule,
-    AdminModule,
-    ClientModule,
-    NgxScannerQrcodeModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    BrowserModule
   ],
-  providers: [
-    {provide: LocationStrategy, useClass: HashLocationStrategy}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-
-}
+export class AppModule { }
